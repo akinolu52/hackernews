bb26c1f5293d7e2771635373a7ea4ceea0c42a97
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2021-07-12 20:53:14 +0100

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@scenes': './src/scenes',
+          '@utils': './src/utils',
+        },
+      },
+    },
+  },
+};
